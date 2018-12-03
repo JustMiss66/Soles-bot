20e7e53c0da0aa02fb3de79f9a5ce1fd32f98c54
MissNela <44345612+MissNela@users.noreply.github.com>
2018-12-03 23:03:10 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -28,13 +28,30 @@ async def on_ready():
 
 @client.command()
 async def help():
+    embed = discord.Embed(title = "Help commands!", color = 0xBA55D3)
+    embed.set_footer(text = "Bot made by Nela say congrats to her please :)")
+    embed.add_field(name = "]feet", value = "Shows an awesome foot!", inline=False)
+    embed.add_field(name = "]toes", value = "Shows some toes ;)",inline=False)
+    embed.add_field(name = "]cfeet", value = "Shows cum on feet pic/gif! (Preparing)",inline=False)
+    await client.say(embed=embed)
+                     
 
 
 
 
 
 @client.command()
-async def meme():
-         embed = discord.Embed(title="Take my meme!", color = 0xFFD700)
+async def feet():
+         embed = discord.Embed(title="Take my meme!", color = 0xDA70D6)
          embed.set_footer(text="Tip: If the image didnt load try to use this command again! | Developer Nela | Bot version: 1.3")
          embed.set_image(url = random.choice([
+             "https://cdn.psychologytoday.com/sites/default/files/styles/image-article_inline_full/public/blogs/315/2009/11/34495-16628.jpg?itok=Ge_x7sNX",
+             "https://mfiles.alphacoders.com/431/431663.jpg",
+             "https://i.pinimg.com/originals/c2/d2/fd/c2d2fd938526f05e2eb41a9e6012ad3a.jpg",
+             "https://i.pinimg.com/736x/4d/28/e0/4d28e09859b3ea4f6d19dc6876885540.jpg",
+             "https://i.pinimg.com/originals/32/e3/a8/32e3a84b0ce709c6849c6b62cb9108c3.jpg",
+             "https://i.pinimg.com/originals/5f/be/a3/5fbea3dffe6dfb7e3c9325fdceacb2d1.png",
+             "https://i.pinimg.com/originals/10/ed/c9/10edc98f708736f13ff6352f3dac796c.jpg"]))
+        await client.say(embed=embed)
+             
+client.run(os.getenv("BOT_TOKEN"))
