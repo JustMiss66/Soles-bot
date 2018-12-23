87806af18fca425f345127ade9f99e430e708a74
MissNela <44345612+MissNela@users.noreply.github.com>
2018-12-23 23:19:02 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,10 +21,24 @@ client = commands.Bot(command_prefix = ']')
 
 client.remove_command('help')
 
+def dev(m):
+    return m.author == "342364288310312970"
+
 @client.event
 async def on_ready():
     await client.change_presence(game=discord.Game(name= "Prefix: ]"))
     print("The bot is online and connected with Discord!") 
+
+@client.command(pass_context = True)
+@commands.check(dev)
+
+    
+async def restart():
+    
+        
+    await client.logout()
+else:
+    await client.say("You are not my developer!")
 
 @client.command()
 async def help():
@@ -140,5 +154,34 @@ async def on_member_join(user: discord.Member):
     embed.set_thumbnail(url=user.avatar_url)
     await client.send_message(channel, embed=embed)
     
+@client.command(pass_context = True)
+async def play(ctx, *, url):
+    author = ctx.message.author
+    voice_channel = author.voice_channel
+    try:
+        vc = await client.join_voice_channel(voice_channel)
+        msg = await client.say("Loading...")
+        player = await vc.create_ytdl_player("ytsearch:" + url)
+        player.start()
+        await client.say("Succesfully Loaded ur song!")
+        await client.delete_message(msg)
+    except Exception as e:
+        print(e)
+        await client.say("Reconnecting")
+        for x in client.voice_clients:
+            if(x.server == ctx.message.server):
+                await x.disconnect()
+                nvc = await client.join_voice_channel(voice_channel)
+                msg = await client.say("Loading...")
+                player2 = await nvc.create_ytdl_player("ytsearch:" + url)
+                player2.start()
+
+@client.command(pass_context = True)
+async def stop(ctx):
+    for x in client.voice_clients:
+        if(x.server == ctx.message.server):
+            return await x.disconnect()
+
+    return await client.say("I am not playing anyting???!")
 
 client.run(os.getenv("BOT_TOKEN"))
