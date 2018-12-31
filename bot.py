36e521b2b62e5ad5c47b0874112556a6541f1a45
MissNela <44345612+MissNela@users.noreply.github.com>
2018-12-31 03:28:41 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -77,52 +77,41 @@ async def update():
     embed.set_footer(text="Bot made by: Nela | v1.3")
     await client.say(embed=embed)
 
-@client.command()
-async def toes():
-    embed = discord.Embed(title = "Look at this toe!", color = 0xFA8072)
-    embed.set_footer(text=" Made by Nela! | v1.3")
-    embed.set_image(url = random.choice([
-        "https://dss.fosterwebmarketing.com/upload/coachellavalleypodiatrist.com/Claw%20Toe.jpg",
-        "https://i.pinimg.com/236x/76/28/64/76286494f486844549952c19909ea510--sore-feet-big-toe.jpg",
-        "https://img.webmd.com/dtmcms/live/webmd/consumer_assets/site_images/articles/health_tools/common_foot_problems/istock_photo_of_womans_feet.jpg",
-        "https://media4.s-nbcnews.com/j/streams/2013/may/130522/6c7531804-130522-feet-fungus-tease-1020a.nbcnews-ux-1024-900.jpg",
-        "https://cdn.diabetesselfmanagement.com/2015/05/Zimmer051915.jpg",
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcR_Vqf7C3YbCBxMx1dAjGmZCSPK5qPYkLcdzJzCTz4Uw9uFlil7",
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQGmd5sbGsdVPDs6W2p-GqRA3OO385WGbf4-SAOOf9pPp19s-u2",
-        "https://www.bigodino.it/wp-content/uploads/2016/09/Piedi-idratati-tutto-lanno.jpg",
-        "http://resize3-doctissimocom.ladmedia.fr/r/940,,force/crop/940,470/img/var/doctissimo/storage/images/common/belleza/novias/cuidados-antes-de-la-boda/pies-perfectos-el-dia-de-tu-boda/347277-1-esl-ES/luce-unos-pies-perfectos-el-dia-de-tu-boda.jpg"]))
-    await client.say(embed=embed)
-    
+@client.command(pass_context=True)
+async def toes(ctx):
+    colour = '0x' + '008000'
+    async with aiohttp.ClientSession() as session:
+        async with session.get("https://api.reddit.com/r/toes/random") as r:
+            data = await r.json()
+            embed = discord.Embed(title='Wow I like this!', description='from reddit', color=discord.Color(int(colour, base=16)))
+            embed.set_image(url=data[0]["data"]["children"][0]["data"]["url"])
+            embed.set_footer(text=f'Requested by: {ctx.message.author.display_name}', icon_url=f'{ctx.message.author.avatar_url}')
+            embed.timestamp = datetime.datetime.utcnow()
+            await client.say(embed=embed)
+
 @client.command()
 async def cfeet():
-    embed = discord.Embed(title = "Do you like it? 😏", color = 0xFFA07A)
-    embed.set_footer(text=" Made by Nela!| v1.3")
-    embed.set_image(url = random.choice([
-        "https://cdn4.images.motherlessmedia.com/images/5E6F284.jpg?fs=opencloud",
-        "https://cdn4.images.motherlessmedia.com/images/0896404.jpg?fs=opencloud",
-        "http://disney-info.info/images/8eedb10535483b3ea9e93fcb1441d984.jpg",
-        "https://images.sex.com/images/pinporn/2015/10/10/300/13999327.gif",
-        "https://66.media.tumblr.com/0da2d7a94217e4f7572d545b48636251/tumblr_okoi24JdHC1ulnn8jo1_500.gif",
-        "https://cumfoot.files.wordpress.com/2012/04/foot107.gif"]))
-    await client.say(embed=embed)
-        
+    colour = '0x' + '008000'
+    async with aiohttp.ClientSession() as session:
+        async with session.get("https://api.reddit.com/r/cumming_on_feet_gif/random") as r:
+            data = await r.json()
+            embed = discord.Embed(title='Dont tell me you dont like it.', description='from reddit', color=discord.Color(int(colour, base=16)))
+            embed.set_image(url=data[0]["data"]["children"][0]["data"]["url"])
+            embed.set_footer(text=f'Requested by: {ctx.message.author.display_name}', icon_url=f'{ctx.message.author.avatar_url}')
+            embed.timestamp = datetime.datetime.utcnow()
+            await client.say(embed=embed)
+
 @client.command(pass_context=True)
 async def anime_feet(ctx):
-    embed = discord.Embed(color = 0xFF00FF, title = random.choice([
-        "Wow isnt this great?",
-        "If you dont like it then idk what happen to you.."]))
-    embed.set_image(url = random.choice([
-        "https://i.pinimg.com/originals/76/59/97/765997fbe30e3c7ffd7f720c79b8b8d2.png",
-        "https://i.pinimg.com/originals/aa/f5/b8/aaf5b8d355383ad8d0116bdd3bcd4d7f.jpg",
-        "https://www.oyunoyna.us/wp-content/uploads/2018/04/einverstandniserklarung-nutzung-fotos-luxus-wykop-newsy-aktualnoac29bci-gry-wiadomoac29bci-muzyka-of-einverstandniserklarung-nutzung-fotos-1.jpg",
-        "https://c.wallhere.com/photos/a8/aa/anime_anime_girls_underwear_legs_feet_short_hair_blue_hair_blue_eyes-1370773.jpg!d",
-        "http://i.imgur.com/pYuJ5.jpg",
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQl6OthUM2DOK2JvXPGU-fHojvmoBKD21evmetQ3zXh4cIqmcLr",
-        "https://i.pinimg.com/originals/10/0a/24/100a24d66a560feeab6b9526ceb0d548.png",
-        "https://steamusercontent-a.akamaihd.net/ugc/307738934713973831/383AB085056139AE110C306A1AD56873DE66CCBB/"]))
-    
-    embed.set_footer(text = "Requested by {0}".format(ctx.message.author.name))
-    await client.say(embed=embed)
+    colour = '0x' + '008000'
+    async with aiohttp.ClientSession() as session:
+        async with session.get("https://api.reddit.com/r/anime_feet/random") as r:
+            data = await r.json()
+            embed = discord.Embed(title='I like Anime!', description='from reddit', color=discord.Color(int(colour, base=16)))
+            embed.set_image(url=data[0]["data"]["children"][0]["data"]["url"])
+            embed.set_footer(text=f'Requested by: {ctx.message.author.display_name}', icon_url=f'{ctx.message.author.avatar_url}')
+            embed.timestamp = datetime.datetime.utcnow()
+            await client.say(embed=embed)
  
 #Logs are down ↓ #          
 
@@ -207,7 +196,7 @@ async def on_member_remove(user: discord.Member):
     joined = user.joined_at
     uid = user.id
     tma = user.avatar_url
-    embed = discord.Embed(title="Member left!", color = 0x00FF0C)
+    embed = discord.Embed(title="Member left!", color = 0xA52A2A)
     embed.add_field(name="Member Profile Picture:",value="Is on right side if no then he have default profile picture.",inline=False)
     embed.add_field(name = "Member Name:",value="{0}".format(name), inline=False)
     embed.add_field(name = "Member ID:",value = "{0}".format(uid), inline=False)
