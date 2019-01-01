72b8b05247487f813b5b5b1f38ca8285490102cc
MissNela <44345612+MissNela@users.noreply.github.com>
2019-01-01 04:34:00 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -209,7 +209,7 @@ async def on_member_remove(user: discord.Member):
 @client.command(pass_context=True)
 async def slap(ctx):
     embed = discord.Embed(title = "Wow", color = 0x000000)
-    embed.set_image(urle = random.choice([
+    embed.set_image(url = random.choice([
         "https://cdn.discordapp.com/attachments/526206250363519016/529497927924580373/Witch-slap-umineko-no-naku-koro-ni-32769184-300-170.gif",
         "https://cdn.discordapp.com/attachments/526206250363519016/529496839637041184/giphy.gif"]))
     await client.send_message(ctx.message.channel, embed=embed)
