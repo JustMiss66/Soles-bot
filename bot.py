f57d01ae1617b74599cce1a7527e4ec4ef86e7f5
JustMiss66 <46427504+JustMiss66@users.noreply.github.com>
2019-01-13 22:26:01 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -65,36 +65,6 @@ async def on_member_join(member):
         with open("users.json", "w") as f:
             json.dump(users, f)
 
-@client.event
-async def on_message(message):
-    with open("users.json", "r") as f:
-        users = json.load(f)
-
-        if message.author.bot:
-            return
-        else:
-            await update_data(users, message.author)
-            number = random.randint(5,10)
-            await add_experience(users, message.author, number)
-            await level_up(users, message.author, message.channel)
-
-        with open("users.json", "w") as f:
-            json.dump(users, f)
-    await client.process_commands(message)
-
-async def update_data(users, user):
-    if not user.id in users:
-        users[user.id] = {}
-        users[user.id]["experience"] = 0
-        users[user.id]["level"] = 0
-
-async def add_experience(users, user, exp):
-    users[user.id]["experience"] += exp
-
-async def level_up(users, user, channel):
-    experience = users[user.id]["experience"]
-    lvl_start = users[user.id]["level"]
-    lvl_end = int(experience ** (0/4))
 
     if lvl_start < lvl_end:
         await client.send_message(channel, f":tada: | Congratulation {user.mention}, you levelled up to level {lvl_end}!")
